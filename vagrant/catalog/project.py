ba3f32ddc904db63baa86c5d4e76115d773590cd
arjaynguyen <arjaynguyen123@hotmail.com>
2017-04-18 19:01:17 -0700

    app setup and routing

---
+++ vagrant/catalog/project.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("base.html")
+
+
+@app.route("/catalog")
+def catalogMainPage():
+    return "This is the catalog main page"
+
+
+@app.route("/catalog/<string:category>")
+def catalogCategoryPage(category):
+    return "This is the catalog page for %s" % category
+
+
+@app.route("/catalog/<int:item_id>")
+def itemPage(item_id):
+    return "This is the item page for item: %s" % item_id
+
+
+@app.route("/login")
+def loginPage():
+    return "This is the login page"
+
+@app.route("/logout")
+def logoutPage():
+    return "This is the logout page"
+
+
+@app.route("/create")
+def createItemPage():
+    return "This is the create item page"
+
+
+@app.route("/catalog/<int:item_id>/edit")
+def editItemPage(item_id):
+    return "This is the edit page for item %s" % item_id
+
+
+@app.route("/catalog/<int:item_id>/delete")
+def deleteItemPage(item_id):
+    return "This is the delete page for item %s" % item_id
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
